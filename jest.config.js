73596b3485c83b7c7aa9c6edffdf5b1f421f4382
Clara Castillo <claracastillochale@gmail.com>
2022-11-10 12:58:11 +0100

    chore: Add project structure and config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const jestConfig = {
+  displayName: 'jikan-ts',
+  clearMocks: true,
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/src/models/**',
+    '!**/src/constants/**',
+    '!**/src/config/**',
+    '!**/src/structures/**',
+  ],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
+
+module.exports = jestConfig;
