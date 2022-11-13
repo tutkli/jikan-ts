9c930cf08180a36e731f1f1cbfc9f83b0a995fc7
Clara Castillo <claracastillochale@gmail.com>
2022-11-13 10:04:49 +0100

    fix: pino imports

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,25 +1,25 @@
-import pino, { DestinationStream, Logger, LoggerOptions } from 'pino';
+import pino from 'pino';
 import { AxiosError } from 'axios';
 import { CacheAxiosResponse, CacheRequestConfig } from 'axios-cache-interceptor';
 
-export const createLogger = (options: LoggerOptions | DestinationStream): Logger => pino(options);
+export const createLogger = (options: pino.LoggerOptions | pino.DestinationStream): pino.Logger => pino(options);
 
-export const handleRequest = (config: CacheRequestConfig, logger: Logger): CacheRequestConfig => {
+export const handleRequest = (config: CacheRequestConfig, logger: pino.Logger): CacheRequestConfig => {
   logger.info(`[ Request Config ] ${config.method?.toUpperCase() || ''} | ${config.url || ''}`);
   return config;
 };
 
-export const handleRequestError = (error: AxiosError, logger: Logger): Promise<AxiosError> => {
+export const handleRequestError = (error: AxiosError, logger: pino.Logger): Promise<AxiosError> => {
   logger.error(`[ Request Error] CODE ${error.code || 'UNKNOWN'} | ${error.message}`);
   throw error;
 };
 
-export const handleResponse = (response: CacheAxiosResponse, logger: Logger): CacheAxiosResponse => {
+export const handleResponse = (response: CacheAxiosResponse, logger: pino.Logger): CacheAxiosResponse => {
   logger.info(response.data);
   return response;
 };
 
-export const handleResponseError = (error: AxiosError, logger: Logger): Promise<AxiosError> => {
+export const handleResponseError = (error: AxiosError, logger: pino.Logger): Promise<AxiosError> => {
   logger.error(`[ Response Error ] CODE ${error.code || 'UNKNOWN'} | ${error.message}`);
   throw error;
 };
