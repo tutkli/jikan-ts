f93d33a649c47957a72da84bd95848973532baef
Clara Castillo <claracastillochale@gmail.com>
2022-12-28 19:28:41 +0100

    build: don't add external dependencies in built package

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,14 +1,17 @@
 import { build } from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
 build({
   entryPoints: ['src/index.ts'],
   outfile: 'dist/index.js',
+  platform: 'node',
   bundle: true,
   minify: true,
   format: 'esm',
   globalName: 'jikants',
   loader: { '.ts': 'ts' },
   tsconfig: 'tsconfig.json',
+  plugins: [nodeExternalsPlugin()],
 })
   .then(() => console.log('⚡ Done'))
   .catch(() => process.exit(1));
