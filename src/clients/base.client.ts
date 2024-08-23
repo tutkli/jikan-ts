b8b5468df29a8bd99e3e618820982e41dd44a191
Clara Castillo <claracastillochale@gmail.com>
2024-08-23 18:54:39 +0200

    feat: replace axios with xior

--- src/clients/base.client.ts
+++ src/clients/base.client.ts
@@ -1,17 +1,4 @@
-import axios, { type AxiosError } from 'axios'
-import {
-	type AxiosCacheInstance,
-	type CacheAxiosResponse,
-	type CacheOptions,
-	type InternalCacheRequestConfig,
-	setupCache
-} from 'axios-cache-interceptor'
-import {
-	handleRequest,
-	handleRequestError,
-	handleResponse,
-	handleResponseError
-} from '../config'
+import xior, { type XiorInstance } from 'xior'
 import { BaseURL } from '../constants'
 import type { JikanResponse } from '../models'
 
@@ -21,17 +8,6 @@ import type { JikanResponse } from '../models'
  * Used to pass optional configuration for logging and cache to the clients.
  */
 export interface ClientArgs {
-	/**
-	 * **EnableLogging**
-	 * Enables logging request responses.
-	 */
-	enableLogging: boolean
-	/**
-	 * **Axios Cache Options**
-	 * Options for cache.
-	 * @see https://axios-cache-interceptor.js.org/#/pages/configuration
-	 */
-	cacheOptions: Partial<CacheOptions>
 	/**
 	 * **Base URL**
 	 * Location of the JikanAPI. Leave empty to use the official JikanAPI instance.
@@ -45,22 +21,15 @@ export interface ClientArgs {
  * This client is responsible for creating an Axios Instance and the cache and logging configurations
  */
 export abstract class BaseClient {
-	private api: AxiosCacheInstance
+	private api: XiorInstance
 
 	constructor(clientOptions: Partial<ClientArgs> = {}) {
-		this.api = setupCache(
-			axios.create({
-				baseURL: clientOptions.baseURL ?? BaseURL,
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}),
-			{ ...clientOptions.cacheOptions, cacheTakeover: false }
-		)
-
-		if (clientOptions.enableLogging) {
-			this.addLoggingInterceptors()
-		}
+		this.api = xior.create({
+			baseURL: clientOptions.baseURL ?? BaseURL,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
 	}
 
 	protected async getResource<T>(
@@ -89,17 +58,5 @@ export abstract class BaseClient {
 			endpoint = endpoint.replace(`{${param}}`, String(params[param]))
 		}
 		return endpoint
-	}
-
-	private addLoggingInterceptors(): void {
-		this.api.interceptors.request.use(
-			(config: InternalCacheRequestConfig) => handleRequest(config),
-			(error: AxiosError<string>) => handleRequestError(error)
-		)
-
-		this.api.interceptors.response.use(
-			(response: CacheAxiosResponse) => handleResponse(response),
-			(error: AxiosError<string>) => handleResponseError(error)
-		)
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from './clients'
-export * from './config'
 export * from './constants'
 export * from './models'
