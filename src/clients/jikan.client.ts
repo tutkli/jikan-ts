9c71ff513afc1aa01b0dc34b444aaa1bef6473b1
dady8889 <dady8889@gmail.com>
2024-12-27 03:50:47 +0100

    fix: use axios instance factory for JikanClient

--- src/clients/jikan.client.ts
+++ src/clients/jikan.client.ts
@@ -1,5 +1,5 @@
+import { AxiosInstance } from 'axios'
 import { AnimeClient } from './anime.client'
-import type { ClientArgs } from './base.client'
 import { CharactersClient } from './characters.client'
 import { GenresClient } from './genres.client'
 import { MangaClient } from './manga.client'
@@ -7,6 +7,34 @@ import { RandomClient } from './random.client'
 import { SchedulesClient } from './schedules.client'
 import { SeasonsClient } from './seasons.client'
 import { TopClient } from './top.client'
+import { CacheOptions } from 'axios-cache-interceptor'
+
+export interface JikanClientArgs {
+	/**
+	 * **EnableLogging**
+	 * Enables logging request responses.
+	 */
+	enableLogging: boolean
+
+	/**
+	 * **Axios Cache Options**
+	 * Options for cache.
+	 * @see https://axios-cache-interceptor.js.org/#/pages/configuration
+	 */
+	cacheOptions: Partial<CacheOptions>
+
+	/**
+	 * **Base URL**
+	 * Location of the JikanAPI. Leave empty to use the official JikanAPI instance.
+	 */
+	baseURL: string
+
+	/**
+	 * **Axios Instance Factory**
+	 * The ability to build your own axios instance if you need it
+	 */
+	axiosInstanceFactory?: () => AxiosInstance
+}
 
 /**
  * **Jikan Client**
@@ -25,14 +53,38 @@ export class JikanClient {
 	public seasons: SeasonsClient
 	public random: RandomClient
 
-	constructor(clientOptions?: Partial<ClientArgs>) {
-		this.anime = new AnimeClient(clientOptions)
-		this.characters = new CharactersClient(clientOptions)
-		this.genres = new GenresClient(clientOptions)
-		this.manga = new MangaClient(clientOptions)
-		this.top = new TopClient(clientOptions)
-		this.schedules = new SchedulesClient(clientOptions)
-		this.seasons = new SeasonsClient(clientOptions)
-		this.random = new RandomClient(clientOptions)
+	constructor(clientOptions?: Partial<JikanClientArgs>) {
+		this.anime = new AnimeClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.characters = new CharactersClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.genres = new GenresClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.manga = new MangaClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.top = new TopClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.schedules = new SchedulesClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.seasons = new SeasonsClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
+		this.random = new RandomClient({
+			...clientOptions,
+			axiosInstance: clientOptions?.axiosInstanceFactory?.()
+		})
 	}
 }
