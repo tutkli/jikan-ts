c9c47d3519a504070816606469afe65a1e399874
Clara Castillo <claracastillochale@gmail.com>
2026-03-21 21:26:46 +0100

    refactor: modularize client, types, and config exports

--- bun.build.js
+++ bun.build.js
@@ -1,13 +1,10 @@
-import dts from 'bun-plugin-dts'
-
 await Bun.build({
-	entrypoints: ['./src/index.ts'],
+	entrypoints: ['./src/index.ts', './src/types.ts', './src/client.ts'],
 	outdir: './dist',
 	splitting: true,
 	external: ['axios', 'axios-cache-interceptor'],
 	sourcemap: 'none',
 	format: 'esm',
 	target: 'node',
-	minify: true,
-	plugins: [dts()]
+	minify: true
 })
